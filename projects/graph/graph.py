21f8b7029fea2d7fedb9b3f4c7ae10c7bb2110bd
Diana Ignatescu <diana.ignatescu8@gmail.com>
2019-06-17 14:00:06 +0100

    Implement add_vertex & add_edge following lecture

--- projects/graph/graph.py
+++ projects/graph/graph.py
@@ -3,20 +3,29 @@ Simple graph implementation
 """
 from util import Stack, Queue  # These may come in handy
 
+
 class Graph:
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
     def __init__(self):
         self.vertices = {}
+
     def add_vertex(self, vertex):
         """
         Add a vertex to the graph.
         """
-        pass  # TODO
+        self.vertices[vertex] = set()
+
     def add_edge(self, v1, v2):
         """
         Add a directed edge to the graph.
         """
-        pass  # TODO
+        # check if at least two vertices
+        if v1 in self.vertices and v2 in self.vertices:
+            # add v2 to the vertices at v1
+            self.vertices[v1].add(v2)
+        else:
+            raise IndexError("The vertex does not exist")
+
     def bft(self, starting_vertex):
         """
         Print each vertex in breadth-first order
